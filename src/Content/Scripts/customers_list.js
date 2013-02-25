68e0c378a46053ee8da95e53d2b49a27a5169004
unknown <wtsams@wtsams.corp.tangeroutlet.com>
2013-02-25 17:12:37 -0500

    Cleaning up JS globals.

---
+++ src/Content/Scripts/customers_list.js
@@ -0,0 +1,25 @@
+﻿var CustomersListJS = function (commonJS, appPath) {
+    var me = this;
+
+    this.AppPath = appPath;
+    this.CommonJS = commonJS;
+
+    this.getClassification = function (cellValue, options, rowdata, action) {
+        //just a simple classification for the example calculate based on customer's A alphabet count
+        var aCount = me.CommonJS.getCharCount(rowdata.ContactName, 'a');
+        if (aCount > 4) { return 'A'; }   //great customer!
+        if (aCount > 1) { return 'B'; }   //ok...
+
+        return 'C';   //a really lousy customer
+    };
+
+    this.customerLink = function (cellValue, options, rowdata, action) {
+        return "<a href='" + me.AppPath + "/Customers/Browse/" + rowdata.CustomerId +
+           "' >" + rowdata.ContactName + "</a>";
+    };
+
+    this.ordersLink = function (cellValue, options, rowdata, action) {
+        return "<a href='" + me.AppPath + "/Orders/Process/" + rowdata.CustomerId +
+                "/" + rowdata.ContactName + "'>Orders</a>";
+    }
+};
---
+++ src/Content/Scripts/orders_process.js
@@ -0,0 +1,10 @@
+﻿var OrdersProcessJS = function (commonJS, appPath) {
+    var me = this;
+
+    this.AppPath = appPath;
+    this.CommonJS = commonJS
+
+    this.FormatDate = function(cellValue, options, rowdata, action) {
+        return me.CommonJS.formatJSONDate(rowdata.OrderDate);
+    }
+};
